d99ec9bf69e767fe6140b0d04c02ddf6e4ad940b
SkariieOff <44173772+SkariieOff@users.noreply.github.com>
2018-11-07 15:33:50 +0100

    Update clear.js

--- .gitignore/commandes/clear.js
+++ .gitignore/commandes/clear.js
@@ -14,7 +14,7 @@ module.exports.run = async (bot, message, args) => {
         .setColor("0wFF8000")
         .setTimestamp(new Date())
         .setFooter(`KokaBot | Exécuté par  ${message.author.tag}`,"https://cdn.discordapp.com/icons/488094402947055647/049024a47a53c3a99d0dff94deb711d5.webp")
-    message.guild.channels.get('505732469312716814').send(clearlog)   
+    message.guild.channels.get('509737142398877697').send(clearlog)   
     }); 
 }
 
