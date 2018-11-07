18ca454c28e8fb4125186a8591ab1820a38c688f
SkariieOff <44173772+SkariieOff@users.noreply.github.com>
2018-11-07 15:08:15 +0100

    Create help.js

---
+++ .gitignore/commandes/help.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const prefix = require('../prefix.json')
+
+module.exports.run = (bot, message, args) => {
+    if (!message.content.startsWith('!')) return;
+      let px = prefix.prefix
+      const help = new Discord.RichEmbed()
+          .setAuthor("Menu d'aide")
+          .setDescription("Le but du menu d'aide est de vous aider (d'où le nom du menu)")
+          .addField(`${px}help`,"Affiche ce menu d'aide.")
+          .addField(`${px}clear <nombre>`,"Suprime un certains nombre de message.")
+          .addField(`${px}say <texte>`,"Il répéte ce que vous dîtes.")
+          .addField(`${px}kick <mention user> <texte>`,"Pour expulser un membre du serveur Discord.")
+          .addField(`${px}ban <mention user> <texte>`,"Pour bannir un membre du serveur Discord.")
+          .setColor("0wFF8000")
+          .setFooter(`KokaBot | Requête de ${message.author.tag}`,"http://kokazia.tk/app/webroot/img/uploads/icon-server/Logo.png")
+      //if(!args[0]) message.guild.channels.get('505733432027119626').send(help)
+      if(!args[0]) message.author.send(help)
+      message.delete()
+      message.reply('la liste des commandes vous a bien été envoyé en message privé !').then(confirmmsg => {
+      confirmmsg.react("✉")
+      setTimeout(() => {
+      confirmmsg.delete()
+      }, 3000);});
+}
+
+
+module.exports.help = {
+    name: "help"
+}
